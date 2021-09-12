bed930aaa0fdfc0d28a4d43ff7e7a98b41166722
AkioSarkiz <akiosarkiz@gmail.com>
2021-09-12 22:33:04 +0300

    try add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    base: '/openapi-attributes/',
+    lang: 'en-US',
+    title: 'Php openapi attributes',
+
+    themeConfig: {
+        logo: 'images/openapi-logo.png',
+
+        navbar: [
+            {
+                text: 'Github',
+                link: 'https://github.com/AkioSarkiz/openapi-attributes',
+            },
+        ],
+
+        sidebar: [
+            '/',
+            {
+                text: 'Guide',
+                link: '/guide/',
+                children: [
+                    '/getting-started',
+                ],
+            },
+        ],
+        displayAllHeaders: true,
+        sidebarDepth: 2,
+    },
+};
